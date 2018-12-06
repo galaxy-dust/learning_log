25a77f8b45fa1e2d23241d1ef032a87a05aae5af
zhang <jeffyu1314@sina.com>
2018-12-06 16:20:15 +0800

    fix some bugs, modify .gitignore and requirements.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -130,18 +130,18 @@ LOGIN_URL = 'users/login/'
 
 #Herku config
 Cwd = os.getcwd()
-if Cwd == '/app' or cwd[:4] == '/tmp':
-	import dj-database-url
+if Cwd == '/app' or Cwd[:4] == '/tmp':
+	import dj_database_url
 	DATABASES={
-		'default':dj-database-url.config(default='postgres://localhost')
+		'default':dj_database_url.config(default='postgres://localhost')
 		}
-		SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 		
-		ALLOWED_HOSTS = ['*']
+	ALLOWED_HOSTS = ['*']
 		
-		BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-		STATIC_ROOT = 'staticfiles'
-		STATIC_DIRS = (
+	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+	STATIC_ROOT = 'staticfiles'
+	STATIC_DIRS = (
 			os.path.join(BASE_DIR, 'static'),
 			)
 		
